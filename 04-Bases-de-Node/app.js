5d00a6f9b78a3d821fde1f22d66fba50c00ebe71
dennysferrer <dennys.ferrer@gmail.com>
2022-01-09 11:56:32 -0500

    Se incluyó la carpeta del capitulo 2 donde se repasaron todos los conceptos avanzados de JavaScript

---
+++ 04-Bases-de-Node/app.js
@@ -0,0 +1,11 @@
+const { crearArchivo } = require ('./helpers/multiplicar.js');
+
+const fs = require('fs');
+console.clear();
+
+const base = 20;
+
+crearArchivo(base)
+
+
+
---
+++ 04-Bases-de-Node/helpers/multiplicar.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const crearArchivo = (base) => {
+    console.clear()
+    console.log("======================");
+    console.log(`TABLA DEL ${base}`);
+    console.log("======================");
+
+    let salida = '';
+    for (let i=0; i<=10; i++){
+        salida += `${base} * ${i} = ${base*i}\n`;
+    }
+    console.log(salida);
+    fs.writeFileSync(`tabla-${base}.txt`, salida);
+
+}
+
+module.exports = {
+    crearArchivo
+}
