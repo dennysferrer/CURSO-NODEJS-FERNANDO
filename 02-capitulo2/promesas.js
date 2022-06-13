b81cffb0656b06749eca8ab4eac2a7f5502abf7b
dennysferrer <dennys.ferrer@gmail.com>
2022-06-12 21:54:58 -0500

    Se modifica el archivo de promesas para retomar estudios en nodejs

--- 02-capitulo2/promesas.js
+++ 02-capitulo2/promesas.js
@@ -9,43 +9,11 @@ const Salarios = [
     { id:2, salario: 1500}
 ];
 
-const id = 2;
-
-const getEmpleado = (id) => {
-    
-    const promesaEmpleado = new Promise((resolve, reject) => {
-        const empleado = Empleados.find(e => e.id === id)
-        if (empleado){
-            resolve(empleado)
-        } else {
-            reject(`El empleado con id ${id} no existe`);
-        }
-    })
-    return promesaEmpleado;  
+const getEmpleado = (id, callback) => {
+    const empleado = Empleados.find(e => e.id === id)
+    if (empleado){
+        callback(null, empleado);
+    } else {
+        console.log(`El empleado con id ${id} no existe`);
+    }    
 }
-
-const getSalario = (id, callback) => {
-
-    const promesaSalario = new Promise((resolve, reject) => {
-        const salario = Salarios.find(s => s.id === id);
-        if (salario){
-            resolve(salario);
-        } else {
-            reject(`El salario con id ${id} no existe`);
-        }
-    })
-    return promesaSalario;
-}
-
-getEmpleado(id)
-    .then(empleado => {
-        console.log(empleado.nombre);
-        getSalario(id)
-            .then(salario => console.log(salario.salario))
-            .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
-
-
-
-
