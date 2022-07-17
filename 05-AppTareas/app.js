b3e2aab58cdb2c334f25af393ef563a4258d35e9
dennysferrer <dennys.ferrer@gmail.com>
2022-07-17 16:30:04 -0500

    Se trabaja en la primera versión de la aplicación de tareas elaborando la interfaz con readline y usando promesas

--- 05-AppTareas/app.js
+++ 05-AppTareas/app.js
@@ -1,20 +1,19 @@
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { mostrarMenu, pausa } = require('./helpers/mensajes')
 
 console.clear()
 
-let opt = '';
-
 const main = async() => {
+
+    let opt = '';
+
     do {
-         opt = await mostrarMenu();
-         console.log({opt});
-         if (opt !== '0'){
-            await pausa();
-         }
-         
-    } while(opt !== '0');
+        opt = await mostrarMenu();
+        console.log(opt);
+        await pausa()
+    } while (opt !== '0')
+
+
 }
 
 main();
-
--- 05-AppTareas/helpers/mensajes.js
+++ 05-AppTareas/helpers/mensajes.js
@@ -1,48 +1,53 @@
+const { read } = require('fs');
+const { resolve } = require('path');
+
 require('colors');
 
 const mostrarMenu = () => {
 
-    return new Promise ( resolve => {
+    return new Promise( resolve => {
         console.clear();
-        console.log('============================='.green);
-        console.log('   Selecciona una opción'.blue);
-        console.log('============================='.green);
+        console.log("========================".green);
+        console.log("Seleccione una Opción".green);
+        console.log("========================\n".green);
 
-        console.log(`${'1.'.green} Crear Tarea`);
-        console.log(`${'2.'.green} Listar Tareas`);
-        console.log(`${'3.'.green} Listar Tareas Completadas`);
-        console.log(`${'4.'.green} Listar Tareas Pendientes`);
-        console.log(`${'5.'.green} Completar Tarea(s)`);
-        console.log(`${'6.'.green} Borrar Tarea`);
-        console.log(`${'0.'.green} Salir \n`);
+        console.log(`${'1'.green}. Crear Tareas`);
+        console.log(`${'2'.green}. Listar Tareas`);
+        console.log(`${'3'.green}. Listar Tareas Completadas`);
+        console.log(`${'4'.green}. Listar Tareas Pendientes`);
+        console.log(`${'5'.green}. Completar Tarea(s)`);
+        console.log(`${'6'.green}. Borrar Tarea`);
+        console.log(`${'0'.green}. Salir\n`);
 
         const readline = require('readline').createInterface({
             input: process.stdin,
             output: process.stdout
         });
-
         readline.question('Seleccione una opción: ', (opt) => {
-            resolve(opt);
             readline.close();
-        });   
+            resolve(opt);
+        })
+    })
+}
+
+const pausa = () => {
+
+    return new Promise(resolve => {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        readline.question(`\n Presiones ${'ENTER'.green} para continuar \n`, () => {
+            readline.close();
+            resolve();
+        })
     })
     
 }
 
-const pausa = () => {
-    return new Promise( resolve => {
-        const readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        readline.question(`Presiones ${'ENTER'.green} para continuar ...`, (opt) => {
-            readline.close();
-            resolve();
-        });
-    })
-}
 
 module.exports = {
     mostrarMenu,
     pausa
 }
+
